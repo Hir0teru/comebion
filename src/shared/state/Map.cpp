62f03b8511b670bceffab8bab8398768d02dfa9a
bob <bob@yopmail.com>
2018-10-17 20:02:23 +0200

    finished map for now

--- src/shared/state/Map.cpp
+++ src/shared/state/Map.cpp
@@ -2,7 +2,15 @@
 
 using namespace state;
 
-Map::Map(){}
+Map::Map(){
+  floors.reserve(4);
+  floors[0].reset(new Floor(0, 1)); //floor number, floor element
+  floors[1].reset(new Floor(1, 2));
+  floors[2].reset(new Floor(2,3));
+  floors[3].reset(new Floor(3,4));
+  currentFloor = 0;
+}
+
 
 Map::~Map()
 {
@@ -18,5 +26,8 @@ int Map::GetCurrentFloor(){
 }
 
 void Map::SetCurrentFloor (int newFloor){
+  if (newFloor<0 || newFloor > 3){
+    throw "Not a valid value for floor";
+  }
   this->currentFloor = newFloor;
 }
