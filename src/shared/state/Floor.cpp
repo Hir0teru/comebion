d35672846463aa34c0780c4ae420199655be44d2
bob <bob@yopmail.com>
2018-10-17 20:10:30 +0200

    finished floor for now

--- src/shared/state/Floor.cpp
+++ src/shared/state/Floor.cpp
@@ -17,31 +17,43 @@ Floor::Floor()
 
 }
 
-Floor::Floor (int floorNumber, int element): floorNumber(floorNumber), element(element){
-  int randomNumber;
-  Room* room;
-  std::vector<std::shared_ptr<Enemy>> enemies;
-  enemies.push_back((std::shared_ptr<Enemy>) new Enemy());
-  room = new EnemyRoom(element, enemies);
-  this->firstRoom.reset(room);
-  this->currentRoom.reset(room);
-  for (int i = 1; i<9; i++){
-    randomNumber = rand() % 100;
-    if (randomNumber < 60){
+Floor::Floor (int floorNumber, int element){
+  if (floorNumber < 0 || floorNumber > 3){
+    throw "Not a valid floor number. Value must be between 0 and 3";
+  }
+  else{
+    if (element < 0 || element > 4){
+      throw "Not a valid element. Value must be between 0 and 4.";
+    }
+    else{
+      this -> floorNumber = floorNumber;
+      this -> element = element;
+      int randomNumber;
+      Room* room;
       std::vector<std::shared_ptr<Enemy>> enemies;
       enemies.push_back((std::shared_ptr<Enemy>) new Enemy());
       room = new EnemyRoom(element, enemies);
-    } else if (randomNumber < 80){
-      room = new SleepRoom(element, randomNumber-60);
-    } else {
-      std::vector<std::shared_ptr<Card>> reward;
-      reward.push_back((std::shared_ptr<Card>) new Card("reward1", 0));
-      reward.push_back((std::shared_ptr<Card>) new Card("reward2", 0));
-      reward.push_back((std::shared_ptr<Card>) new Card("reward3", 0));
-      room = new SpecialTrainingRoom(element, reward);
+      this->firstRoom.reset(room);
+      this->currentRoom.reset(room);
+      for (int i = 1; i<9; i++){
+        randomNumber = rand() % 100;
+        if (randomNumber < 60){
+          std::vector<std::shared_ptr<Enemy>> enemies;
+          enemies.push_back((std::shared_ptr<Enemy>) new Enemy());
+          room = new EnemyRoom(element, enemies);
+        } else if (randomNumber < 80){
+          room = new SleepRoom(element, randomNumber-60);
+        } else {
+          std::vector<std::shared_ptr<Card>> reward;
+          reward.push_back((std::shared_ptr<Card>) new Card("reward1", 0));
+          reward.push_back((std::shared_ptr<Card>) new Card("reward2", 0));
+          reward.push_back((std::shared_ptr<Card>) new Card("reward3", 0));
+          room = new SpecialTrainingRoom(element, reward);
+        }
+        currentRoom->SetNextRoom((std::shared_ptr<Room>) room);
+        currentRoom.reset(room);
+      }
     }
-    currentRoom->SetNextRoom((std::shared_ptr<Room>) room);
-    currentRoom.reset(room);
   }
 }
 
