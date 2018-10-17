c41d2a228620110550f09093108807fa80a4785b
bob <bob@yopmail.com>
2018-10-17 09:40:29 +0200

    EnemySkill

--- src/shared/state/EnemySkill.cpp
+++ src/shared/state/EnemySkill.cpp
@@ -1,49 +1,49 @@
-//
-// #include "EnemyRoom.h"
-// using namespace state;
-//
-// EnemySkill::EnemySkill (){}
-// EnemySkill::EnemySkill (int attack, int block, std::vector<std::shared_ptr<Buff>> buffs,
-//    std::vector<std::shared_ptr<Debuff>> debuffs, std::string intentimage, int cooldown){
-//      this -> attack = attack;
-//      this -> block = block;
-//      this -> buffs = buffs;
-//      this -> debuffs = debuffs;
-//      this -> intentImage = intentimage;
-//      this -> cooldown = cooldown;
-//      turnsBeforeUse = 0;
-//    }
-//
-//
-//
-// //enemySkill();
-// int EnemySkill::GetAttack (){
-//   return attack;
-// }
-// void EnemySkill::SetAttack (int newAttack){
-//   attack = newAttack;
-// }
-// int EnemySkill::GetBlock (){
-//   return block;
-// }
-// void EnemySkill::SetBlock (int newBlock){
-//   block = newBlock;
-// }
-// // std::vector<std::shared_ptr<Buff>> EnemySkill::GetBuffs (){
-// //   return buffs;
-// // }
-// // std::vector<std::shared_ptr<Debuff>> EnemySkill::GetDebuffs (){
-// //   return debuffs;
-// // }
-// std::string EnemySkill::GetIntentImage (){
-//   return intentImage;
-// }
-// int EnemySkill::GetCooldown (){
-//   return cooldown;
-// }
-// int EnemySkill::GetTurnsBeforeUse (){
-//   return turnsBeforeUse;
-// }
-// void EnemySkill::SetTurnsBeforeUse (int newTurnsBU){
-//   turnsBeforeUse = newTurnsBU;
-// }
+
+#include "EnemyRoom.h"
+using namespace state;
+
+EnemySkill::EnemySkill (){}
+EnemySkill::EnemySkill (int attack, int block, Buff buffs,
+   Debuff debuffs, std::string intentimage, int cooldown){
+     this -> attack = attack;
+     this -> block = block;
+     this -> buff = buffs;
+     this -> debuff = debuffs;
+     this -> intentImage = intentimage;
+     this -> cooldown = cooldown;
+     turnsBeforeUse = 0;
+   }
+
+
+
+//enemySkill();
+int EnemySkill::GetAttack (){
+  return attack;
+}
+void EnemySkill::SetAttack (int newAttack){
+  attack = newAttack;
+}
+int EnemySkill::GetBlock (){
+  return block;
+}
+void EnemySkill::SetBlock (int newBlock){
+  block = newBlock;
+}
+Buff EnemySkill::GetBuffs (){
+  return buff;
+}
+Debuff EnemySkill::GetDebuffs (){
+  return debuff;
+}
+std::string EnemySkill::GetIntentImage (){
+  return intentImage;
+}
+int EnemySkill::GetCooldown (){
+  return cooldown;
+}
+int EnemySkill::GetTurnsBeforeUse (){
+  return turnsBeforeUse;
+}
+void EnemySkill::SetTurnsBeforeUse (int newTurnsBU){
+  turnsBeforeUse = newTurnsBU;
+}
