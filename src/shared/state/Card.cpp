33f3f83ca9078737b498a36d94975a0436cb57e1
hiroteru <charly.bion@ensea.fr>
2018-10-17 09:40:17 +0200

    Card

--- src/shared/state/Card.cpp
+++ src/shared/state/Card.cpp
@@ -1,88 +1,88 @@
-// #include "Card.h"
-//
-// using namespace state;
-//
-// Card::Card (std::string type, std::string element){
-//   name = type;
-//   /// Coût de la carte en energie
-//   cost = 1;
-//   /// Type de cible visée (0: joueur, 1: monstre, 2: tous les monstres, 3: tous les joueurs)
-//   target = 1;
-//   /// Image représentant la carte
-//
-//   /// Element de la carte (air, eau, terre, feu, pas d'element)
-//   element = element;
-//   /// Dégats de la carte
-//   if (type == "basic attack"){
-//     attack = 5;
-//     block = 0;
-//     image = "/home/ombre/Documents/Projet/comebion/res/textures/cards/bending/air_bending/1.png";
-//   }
-//   /// Nombre de block créé par la carte (à destination du joueur)
-//   else {
-//     block = 5;
-//     attack = 0;
-//     image = "/home/ombre/Documents/Projet/comebion/res/textures/cards/bending/air_bending/2.png";
-//   }
-//
-//   /// Nombre de cartes pichées
-//   draw = 0;
-//   /// Nombre de cartes défaussées
-//   discard = 0;
-//   /// nombre de point de vie du joueur soignés
-//   heal = 0;
-//   /// liste de débuffs appliqués
-//   // debuffs = std::vector<std::shared_ptr<Debuff>> ();
-//   // /// liste de buffs appliqués
-//   // buffs = std::vector<std::shared_ptr<Buff>> ();
-// };
-//
-// int Card::GetCost (){
-//   return cost;
-// };
-//
-// void Card::SetCost (int cost){
-//   cost = cost;
-// };
-//
-// int Card::GetTarget (){
-//   return target;
-// };
-//
-// std::string Card::GetImage (){
-//   return image;
-// };
-//
-// std::string Card::GetElement (){
-//   return element;
-// };
-//
-// int Card::GetAttack (){
-//   return attack;
-// };
-//
-// int Card::GetBlock (){
-//   return block;
-// };
-//
-// int Card::GetDraw (){
-//   return draw;
-// };
-//
-// int Card::GetDiscard (){
-//   return discard;
-// };
-//
-// int Card::GetHeal (){
-//   return heal;
-// };
-//
-// Debuff Card::GetDebuff (){
-//   return debuff;
-// };
-//
-// Buff Card::GetBuff (){
-//   return buff;
-// };
-//
-// // Setters and Getters
+#include "Card.h"
+
+using namespace state;
+
+Card::Card (std::string type, std::string element){
+  name = type;
+  /// Coût de la carte en energie
+  cost = 1;
+  /// Type de cible visée (0: joueur, 1: monstre, 2: tous les monstres, 3: tous les joueurs)
+  target = 1;
+  /// Image représentant la carte
+
+  /// Element de la carte (air, eau, terre, feu, pas d'element)
+  element = element;
+  /// Dégats de la carte
+  if (type == "basic attack"){
+    attack = 5;
+    block = 0;
+    image = "/home/ombre/Documents/Projet/comebion/res/textures/cards/bending/air_bending/1.png";
+  }
+  /// Nombre de block créé par la carte (à destination du joueur)
+  else {
+    block = 5;
+    attack = 0;
+    image = "/home/ombre/Documents/Projet/comebion/res/textures/cards/bending/air_bending/2.png";
+  }
+
+  /// Nombre de cartes pichées
+  draw = 0;
+  /// Nombre de cartes défaussées
+  discard = 0;
+  /// nombre de point de vie du joueur soignés
+  heal = 0;
+  /// liste de débuffs appliqués
+  // debuffs = std::vector<std::shared_ptr<Debuff>> ();
+  // /// liste de buffs appliqués
+  // buffs = std::vector<std::shared_ptr<Buff>> ();
+};
+
+int Card::GetCost (){
+  return cost;
+};
+
+void Card::SetCost (int cost){
+  cost = cost;
+};
+
+int Card::GetTarget (){
+  return target;
+};
+
+std::string Card::GetImage (){
+  return image;
+};
+
+std::string Card::GetElement (){
+  return element;
+};
+
+int Card::GetAttack (){
+  return attack;
+};
+
+int Card::GetBlock (){
+  return block;
+};
+
+int Card::GetDraw (){
+  return draw;
+};
+
+int Card::GetDiscard (){
+  return discard;
+};
+
+int Card::GetHeal (){
+  return heal;
+};
+
+Debuff Card::GetDebuff (){
+  return debuff;
+};
+
+Buff Card::GetBuff (){
+  return buff;
+};
+
+// Setters and Getters
--- src/shared/state/Entity.cpp
+++ src/shared/state/Entity.cpp
@@ -1,98 +1,98 @@
-// #include "Entity.h"
-//
-// using namespace state;
-//
-//
-// Entity::Entity (){}
-// Entity::Entity (std::string name, std::string element, std::string image, int statAttack, int statBlock, bool isPlayer, int life, int id){
-//   this->name = name;
-//   this -> id = id;
-//   this -> isPlayer = isPlayer;
-//   this -> element = element;
-//   this -> image = image;
-//   this -> statAttack = statAttack;
-//   this -> statBlock = statBlock;
-//   this -> life = life;
-//   block = 0;
-//   isEntityAlive = true;
+#include "Entity.h"
+
+using namespace state;
+
+
+Entity::Entity (){}
+Entity::Entity (std::string name, std::string element, std::string image, int statAttack, int statBlock, bool isPlayer, int life, int id){
+  this->name = name;
+  this -> id = id;
+  this -> isPlayer = isPlayer;
+  this -> element = element;
+  this -> image = image;
+  this -> statAttack = statAttack;
+  this -> statBlock = statBlock;
+  this -> life = life;
+  block = 0;
+  isEntityAlive = true;
+}
+
+int Entity::GetLife (){
+  return life;
+}
+
+void Entity::SetLife (int newLife){
+  this->life = newLife;
+}
+
+int Entity::GetId (){
+  return id;
+}
+
+void Entity::SetId (int newId){
+  this->id = newId;
+}
+
+std::string Entity::GetElement (){
+  return element;
+}
+
+void Entity::SetElement (std::string newElement){
+  this->element = newElement;
+}
+
+std::string Entity::GetImage (){
+  return image;
+}
+
+void Entity::SetImage (std::string newImage){
+  this->image = newImage;
+}
+
+int Entity::GetStatAttack (){
+  return statAttack;
+}
+
+void Entity::SetStatAttack (int newStatAttack){
+  this->statAttack = newStatAttack;
+}
+
+int Entity::GetStatBlock (){
+  return statBlock;
+}
+
+void Entity::SetStatBlock (int newStatBlock){
+  this->statBlock = newStatBlock;
+}
+
+// std::vector<std::shared_ptr<Buff>> Entity::GetBuffs (){
+//   return buffs;
 // }
 //
-// int Entity::GetLife (){
-//   return life;
+// void Entity::SetBuffs (std::vector<std::shared_ptr<Buff>> newBuffs){
+//   this->buffs = newBuffs;
 // }
 //
-// void Entity::SetLife (int newLife){
-//   this->life = newLife;
+// std::vector<std::shared_ptr<Debuff>> Entity::GetDebuffs (){
+//   return debuffs;
 // }
 //
-// int Entity::GetId (){
-//   return id;
+// void Entity::SetDebuffs (std::vector<std::shared_ptr<Debuff>> newDebuffs){
+//   this->debuffs = newDebuffs;
 // }
-//
-// void Entity::SetId (int newId){
-//   this->id = newId;
-// }
-//
-// std::string Entity::GetElement (){
-//   return element;
-// }
-//
-// void Entity::SetElement (std::string newElement){
-//   this->element = newElement;
-// }
-//
-// std::string Entity::GetImage (){
-//   return image;
-// }
-//
-// void Entity::SetImage (std::string newImage){
-//   this->image = newImage;
-// }
-//
-// int Entity::GetStatAttack (){
-//   return statAttack;
-// }
-//
-// void Entity::SetStatAttack (int newStatAttack){
-//   this->statAttack = newStatAttack;
-// }
-//
-// int Entity::GetStatBlock (){
-//   return statBlock;
-// }
-//
-// void Entity::SetStatBlock (int newStatBlock){
-//   this->statBlock = newStatBlock;
-// }
-//
-// // std::vector<std::shared_ptr<Buff>> Entity::GetBuffs (){
-// //   return buffs;
-// // }
-// //
-// // void Entity::SetBuffs (std::vector<std::shared_ptr<Buff>> newBuffs){
-// //   this->buffs = newBuffs;
-// // }
-// //
-// // std::vector<std::shared_ptr<Debuff>> Entity::GetDebuffs (){
-// //   return debuffs;
-// // }
-// //
-// // void Entity::SetDebuffs (std::vector<std::shared_ptr<Debuff>> newDebuffs){
-// //   this->debuffs = newDebuffs;
-// // }
-//
-// bool Entity::GetIsPlayer (){
-//   return isPlayer;
-// }
-//
-// void Entity::SetIsPlayer (bool newIsPlayer){
-//   this->isPlayer = newIsPlayer;
-// }
-//
-// int Entity::GetBlock (){
-//   return block;
-// }
-//
-// void Entity::SetBlock (int newBlock){
-//   this->block = newBlock;
-// }
+
+bool Entity::GetIsPlayer (){
+  return isPlayer;
+}
+
+void Entity::SetIsPlayer (bool newIsPlayer){
+  this->isPlayer = newIsPlayer;
+}
+
+int Entity::GetBlock (){
+  return block;
+}
+
+void Entity::SetBlock (int newBlock){
+  this->block = newBlock;
+}
