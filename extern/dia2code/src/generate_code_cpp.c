6c5878f3c1f88824a01d598037336c4afa78df52
bob <bob@yopmail.com>
2018-10-23 15:11:21 +0200

    render

--- extern/dia2code/src/generate_code_cpp.c
+++ extern/dia2code/src/generate_code_cpp.c
@@ -787,7 +787,7 @@ void print_include_stdlib(struct stdlib_includes* si,char* name) {
        && (strstr(name,"sf::RenderWindow")
        ||  strstr(name,"sf::VertexArray")
        ||  strstr(name,"sf::Texture")
-     ||  strstr(name,"sf::RenderTexture")))) {
+       ||  strstr(name,"sf::RenderTexture"))) {
            print ("#include <SFML/Graphics.hpp>\n");
            si->sfmlGraphics = 1;
        }
