159c76bcb08ab65e0dc1127f3474d585e14b9d30
hiroteru <charly.bion@ensea.fr>
2018-11-08 21:08:14 +0100

    correct bug

--- src/shared/engine/CommandAddEnergy.cpp
+++ src/shared/engine/CommandAddEnergy.cpp
@@ -18,7 +18,7 @@ void CommandAddEnergy::Execute (std::shared_ptr<state::GameState>& gameState){
 }
 
 void CommandAddEnergy::Undo (std::shared_ptr<state::GameState>& gameState){
-  if (playerID > 0 && playerID < 2 ){
+  if (entityID > 0 && entityID < 2 ){
     cout<<"Undo Adding "<<energyAmount<< " energy to entity "<<entityID<<"..."<<endl;
     gameState->GetPlayers()[entityID]->SetEnergy(gameState->GetPlayers()[entityID]->GetEnergy() - energyAmount);
   }
